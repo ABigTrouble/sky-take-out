98651b3375a82733e0427f07c074780afbe6331c
root <1724062304@qq.com>
2024-02-08 00:27:31 +0800

    完成 管理端-订单搜索，哥哥状态的订单数量统计，查询订单详情，接单，拒单，取消订单，派送订单，完成订单 功能

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,9 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 
@@ -47,4 +47,33 @@ public interface OrderService {
     void userCancelById(Long id) throws Exception;
 
     void repetition(Long id);
+
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    OrderStatisticsVO statistics();
+
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
+
+    /**
+     * 商家取消订单
+     *
+     * @param ordersCancelDTO
+     */
+    void cancel(OrdersCancelDTO ordersCancelDTO) throws Exception;
+
+    /**
+     * 派送订单
+     *
+     * @param id
+     */
+    void delivery(Long id);
+
+    /**
+     * 完成订单
+     *
+     * @param id
+     */
+    void complete(Long id);
 }
